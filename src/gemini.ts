0a2ec7cf7e258b8bc2384567201888e4fcda402e
Anupat Kaewmee <anupat.k64@rsu.ac.th>
2024-06-27 11:46:31 +0700

    add feture with gemini

--- src/gemini.ts
+++ src/gemini.ts
@@ -1,18 +1,28 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GenerationConfig, GoogleGenerativeAI } from "@google/generative-ai";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const apiKey: string = process.env.GEMINI_API_KEY!;
+const generationConfig: GenerationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
 
 const genAI = new GoogleGenerativeAI(apiKey);
-const baseModel = genAI.getGenerativeModel({ model: "gemini" });
+const baseModel = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const proModel = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
 export const generateText = async (prompt: string) => {
   const result = await baseModel.generateContent(prompt);
-  return result.response.text;
+  return result.response.text();
 };
 
 export const readImage = async (image: string) => {
-  const prompt = "ช่วยบรรยายภาพนี้ใcห้หน่อย";
+  const prompt = "ช่วยบรรยายภาพนี้ให้หน่อย";
   const mimeType = "image/png";
 
   // Convert image binary to a GoogleGenerativeAI.Part object.
@@ -25,29 +35,34 @@ export const readImage = async (image: string) => {
     },
   ];
   const result = await proModel.generateContent([prompt, ...imageParts]);
-  return result.response.text;
+  return result.response.text();
 };
 
 export const chat = async (prompt: string) => {
   const chat = baseModel.startChat({
+    generationConfig,
     history: [
       {
         role: "user",
-        parts: [{ text: "สวัสดีจ้า" }],
+        parts: [{ text: "สวัสดี" }],
       },
       {
         role: "model",
         parts: [
           {
-            text: "I am an expert in Amway products. My name is 'Am'. You can ask me anything! about Amway products.",
+            text: "สวัสดีค่ะ! ฉันคือแมว ต่อไปนี้ฉันจะตอบแค่คำว่า เหมี๊ยว!",
           },
         ],
+      },
+      {
+        role: "user",
+        parts: [{ text: prompt }],
       },
     ],
   });
 
   const result = await chat.sendMessage(prompt);
-  return result.response.text;
+  return result.response.text();
 };
 
 export const chatWithOwnData = async (prompt: string) => {
@@ -79,5 +94,29 @@ export const chatWithOwnData = async (prompt: string) => {
   });
 
   const result = await chat.sendMessage(prompt);
-  return result.response.text;
+  return result.response.text();
+};
+
+export const getImageBinary = async (messageId: string) => {
+  return await axios
+    .get(`https://api-data.line.me/v2/bot/message/${messageId}/content`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+      },
+      responseType: "arraybuffer",
+    })
+    .then((res) => res.data as string);
+};
+
+export const showLoadingAnimation = async (userID: string) => {
+  const response = await axios({
+    method: "post",
+    url: "https://api.line.me/v2/bot/chat/loading/start",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+    },
+    data: { chatId: userID, loadingSeconds: 60 },
+  });
 };
